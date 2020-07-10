f857589298c28f6ebc8633a7fc270e69eaf3670d
AlexanderG <alexandergrau@gmx.de>
2020-07-11 00:06:28 +0200

    config template

--- sunray/config_password_example.h
+++
@@ -1,7 +0,0 @@
-
-
-# IMPORTANT:  PLEASE RENAME THIS FILE INTO 'config_password.h' and restart the Arduino IDE
-
-#define WIFI_SSID "test"        // choose WiFi network ID
-#define WIFI_PASS "test"       // choose WiFi network password
-
