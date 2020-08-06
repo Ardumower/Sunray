940d7b10224d5d7d52b07724707ac960647ca518
AlexanderG <alexandergrau@gmx.de>
2020-08-06 19:53:12 +0200

    add config comment

--- sunray/config_example.h
+++ sunray/config_example.h
@@ -189,7 +189,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 
 // stanley control for path tracking - determines gain how fast to correct for lateral path errors
 #define STANLEY_CONTROL_K_NORMAL  0.5   // 0.5 for path tracking control when in normal or fast motion
-#define STANLEY_CONTROL_K_SLOW    0.1   // 0.1 for path tracking control when in slow motion
+#define STANLEY_CONTROL_K_SLOW    0.1   // 0.1 for path tracking control when in slow motion (e.g. docking tracking)
 
 // ------- I2C addresses -----------------------------
 #define DS1307_ADDRESS B1101000
