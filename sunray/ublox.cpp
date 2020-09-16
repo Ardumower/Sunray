7e30c4e2ef764fc1ccf14d1ee2af34ddddbd2559
AlexanderG <alexandergrau@gmx.de>
2020-09-17 00:08:12 +0200

    gps auto-config (nav input filters)

--- sunray/ublox.cpp
+++ sunray/ublox.cpp
@@ -84,9 +84,18 @@ bool UBLOX::configure(){
   setValueSuccess &= configGPS.addCfgValset8(0x10740004, 0); // CFG-UART1OUTPROT-RTCM3X    
   // uart1 baudrate (Ardumower) 
   setValueSuccess &= configGPS.addCfgValset32(0x40520001, _baud); // CFG-UART1-BAUDRATE  
+  // ----  gps navx5 input filter ----------------------------------
+  //setValueSuccess &= configGPS.addCfgValset8(0x201100a1, 3); // CFG-NAVSPG-INFIL_MINSVS
+  //setValueSuccess &= configGPS.addCfgValset8(0x201100a2, 32); // CFG-NAVSPG-INFIL_MAXSVS
+  //setValueSuccess &= configGPS.addCfgValset8(0x201100a3, 6); // CFG-NAVSPG-INFIL_MINCNO
+  // ----  gps nav5 input filter ----------------------------------
+  setValueSuccess &= configGPS.addCfgValset8(0x201100a4, 10); // CFG-NAVSPG-INFIL_MINELEV
+  setValueSuccess &= configGPS.addCfgValset8(0x201100aa, 10); // CFG-NAVSPG-INFIL_NCNOTHRS
+  setValueSuccess &= configGPS.addCfgValset8(0x201100ab, 30); // CFG-NAVSPG-INFIL_CNOTHRS  
   // ----  gps rates ----------------------------------
   setValueSuccess &= configGPS.addCfgValset16(0x30210001, 200); // CFG-RATE-MEAS       
   setValueSuccess &= configGPS.sendCfgValset16(0x30210002, 1,   2000); //CFG-RATE-NAV
+  
   
   _bus->begin(_baud);
   
