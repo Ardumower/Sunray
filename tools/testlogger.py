2c1f5437a00469cb29074fd8099f0446e2427b7a
alex <alex@grauonline.de>
2020-10-05 08:26:59 +0200

    add logger test

--- tools/testlogger.py
+++ tools/testlogger.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# UDP server - receives UDP packets from Arduino
+# UDP test client - sends UDP packets
 # to be run with Anaconda/Miniconda Python 3.7
 
 import socket
+import time
 
-host = ""
+host = "192.168.2.30"
 port = 4210
 
-bufsize = 99
+message = b'hello world\r\n'
 
 addr = (host, port)
 UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPSock.bind(addr)
-UDPSock.setblocking(0)
-
-print("waiting for messages (port: " + str(port) + ") ...")
 
 # Endlosschleife fuer Empfang, Abbruch durch Strg-C
 try:
   while True:
-    try: 
-      (data, addr) = UDPSock.recvfrom(bufsize)
-    except socket.error:
-      pass
-    else:   
-      print (data.decode('utf-8'), end='', flush=True)      
+    UDPSock.sendto(message, addr)    
+    time.sleep(2.0)
+    
 except KeyboardInterrupt:
   UDPSock.close()
   print ("terminating ...")
   exit(0)
-  
+
