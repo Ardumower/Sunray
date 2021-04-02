e83ebf68342206943afda9279b015c3602b50660
greymfm <alex@grauonline.de>
2021-04-02 11:02:11 +0200

    comments

--- sunray/config_example.h
+++ sunray/config_example.h
@@ -124,9 +124,9 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 
 // motor speed control (PID coefficients) - these values are tuned for Ardumower motors
 // general information about PID controllers: https://wiki.ardumower.de/index.php?title=PID_control
-#define MOTOR_PID_KP     2.0    // do not change
-#define MOTOR_PID_KI     0.03   // do not change
-#define MOTOR_PID_KD     0.03   // do not change
+#define MOTOR_PID_KP     2.0    // do not change 2.0 (for non-Ardumower motors or if the motor speed control is too fast you may try: KP=1.0, KI=0, KD=0)
+#define MOTOR_PID_KI     0.03   // do not change 0.03
+#define MOTOR_PID_KD     0.03   // do not change 0.03
 
 
 // ----- mowing motor -------------------------------------------------
