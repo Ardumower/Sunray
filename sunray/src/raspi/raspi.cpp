16653646b660a10d1f040a78223e74a472959335
greymfm <alex@grauonline.de>
2021-04-10 12:49:37 +0200

    code cleanup for linux

---
+++ sunray/src/raspi/raspi.cpp
@@ -0,0 +1,13 @@
+#include "../../config.h"
+
+#if defined(__linux__)
+
+LinuxSerial Serial1;
+LinuxSerial Serial2;
+LinuxSerial Serial3;
+LinuxSerial Serial4;
+
+
+
+#endif   //  __linux__
+
---
+++ sunray/src/raspi/raspi.h
@@ -0,0 +1,15 @@
+// Raspberry PI
+
+
+#if defined(__linux__)
+
+#include <LinuxSerial.h>
+
+extern LinuxSerial Serial1;
+extern LinuxSerial Serial2;
+extern LinuxSerial Serial3;
+extern LinuxSerial Serial4;
+
+
+#endif  // __linux__
+
