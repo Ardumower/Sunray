cd1771209fc0ffab68300eaa76381614694bbee9
greymfm <alex@grauonline.de>
2022-04-27 18:18:53 +0200

    add code comments

--- sunray/robot.h
+++ sunray/robot.h
@@ -33,33 +33,35 @@
 
 #define VER "Ardumower Sunray,1.0.241"
 
+// operation types
 enum OperationType {
-      OP_IDLE,      
-      OP_MOW,            
-      OP_CHARGE,      
-      OP_ERROR,    
-      OP_DOCK,            
+      OP_IDLE,      // idle
+      OP_MOW,       // mowing
+      OP_CHARGE,    // charging
+      OP_ERROR,     // serious error
+      OP_DOCK,      // go to docking
 };    
 
+// sensor errors
 enum Sensor {
-      SENS_NONE,
-      SENS_BAT_UNDERVOLTAGE,            
-      SENS_OBSTACLE,      
-      SENS_GPS_FIX_TIMEOUT,
-      SENS_IMU_TIMEOUT,
-      SENS_IMU_TILT,
-      SENS_KIDNAPPED,
-      SENS_OVERLOAD,
-      SENS_MOTOR_ERROR,
-      SENS_GPS_INVALID,
-      SENS_ODOMETRY_ERROR,
-      SENS_MAP_NO_ROUTE,
-      SENS_MEM_OVERFLOW,
-      SENS_BUMPER,
-      SENS_SONAR,
-      SENS_LIFT,
-      SENS_RAIN,
-      SENS_STOP_BUTTON,
+      SENS_NONE,              // no error
+      SENS_BAT_UNDERVOLTAGE,  // battery undervoltage
+      SENS_OBSTACLE,          // obstacle triggered
+      SENS_GPS_FIX_TIMEOUT,   // gps fix timeout
+      SENS_IMU_TIMEOUT,       // imu timeout  
+      SENS_IMU_TILT,          // imut tilt
+      SENS_KIDNAPPED,         // robot has been kidnapped (is no longer on planned track)
+      SENS_OVERLOAD,          // motor overload
+      SENS_MOTOR_ERROR,       // motor error
+      SENS_GPS_INVALID,       // gps is invalid or not working
+      SENS_ODOMETRY_ERROR,    // motor odometry error
+      SENS_MAP_NO_ROUTE,      // robot cannot find a route to next planned point
+      SENS_MEM_OVERFLOW,      // cpu memory overflow
+      SENS_BUMPER,            // bumper triggered
+      SENS_SONAR,             // ultrasonic triggered
+      SENS_LIFT,              // lift triggered
+      SENS_RAIN,              // rain sensor triggered
+      SENS_STOP_BUTTON,       // emergency/stop button triggered
 };
 
 #ifndef __linux__
