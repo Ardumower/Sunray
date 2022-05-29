fd062fdda9fef60fbc28e770e93152537965f189
greymfm <alex@grauonline.de>
2022-05-29 10:24:38 +0200

    correct typo

--- sunray/config_example.h
+++ sunray/config_example.h
@@ -1,6 +1,6 @@
 // Ardumower Sunray 
 // Copyright (c) 2013-2020 by Alexander Grau, Grau GmbH
-// Licensed GPLv3 for open soue use
+// Licensed GPLv3 for open source use
 // or Grau GmbH Commercial License for commercial use (http://grauonline.de/cms2/?page_id=153)
 
 /* 
