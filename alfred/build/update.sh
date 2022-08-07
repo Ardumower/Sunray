294e47e5574c75d217611285e3e6449ad9a9914c
hjw2015 <hj.wolff@gmail.com>
2022-08-07 10:23:27 +0200

    Delete update.sh
    not for release

--- alfred/build/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## https://github.com/Ardumower/Sunray/tree/master/alfred
-
-## install new sunray executable ##
-sudo systemctl stop sunray
-cp sunray ~/sunray_install/
-sudo systemctl start sunray
