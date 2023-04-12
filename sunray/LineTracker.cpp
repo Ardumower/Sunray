526246e4d24cb8bda22e5d4e11f9d0d11e488556
Stefan Priebe <stefan-git@prie.be>
2023-04-12 10:19:17 +0200

    LineTracker/config: get_turn_direction_preference: make mower size configurable

--- sunray/LineTracker.cpp
+++ sunray/LineTracker.cpp
@@ -38,7 +38,7 @@ int get_turn_direction_preference() {
   float targetDelta = pointsAngle(stateX, stateY, target.x(), target.y());
   float center_x = stateX;
   float center_y = stateY;
-  float r = 0.3;
+  float r = (MOWER_SIZE / 100);
   float cur_angle = stateDelta;
 
   if (FREEWHEEL_IS_AT_BACKSIDE) {
