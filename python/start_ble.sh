bcf6875e7c0d2eee62b8211d05261a4d89be31cb
greymfm <alex@grauonline.de>
2024-04-23 10:02:07 +0200

    dabble app test code

--- python/start_ble.sh
+++ python/start_ble.sh
@@ -14,7 +14,7 @@ btmgmt -i hci0 le on
 btmgmt -i hci0 bredr off
 btmgmt -i hci0 connectable on
 #btmgmt -i hci0 name "bananapi $MAC"
-btmgmt -i hci0 name "bananapi"
+btmgmt -i hci0 name "owlRobot"
 btmgmt -i hci0 advertising on
 btmgmt -i hci0 power on
 ## btmgmt -i hci0 advinfo
