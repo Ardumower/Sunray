facf8fbeebf74e6ed4e43ae787e230b1b04ab9a7
greymfm <alex@grauonline.de>
2024-05-23 16:36:26 +0200

    add example scripts

---
+++ alfred/service.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+# start/stop sunray service
+# start/stop camera streaming web service on "localhost:8081"
+
+echo "EUID=$EUID"
+echo "PWD=$PWD"
+
+
+CMD=""
+
+
+# start USB camera streaming web server 
+function start_cam_service() {
+  if [[ `pidof motion` != "" ]]; then
+        echo "motion app already running! Exiting..."
+        exit
+  fi
+  echo "starting motion service..."
+  cp config_files/motion/motion.conf /etc/motion
+  cp $PWD/config_files/motion/motion.service /etc/systemd/system/motion.service
+  chmod 644 /etc/systemd/system/motion.service
+  systemctl daemon-reload
+  systemctl enable motion
+  systemctl start motion
+  systemctl --no-pager status motion
+  echo "motion service started!"
+}
+
+function stop_cam_service() {
+  echo "stopping motion service..."
+  systemctl stop motion
+  systemctl disable motion
+  echo "motion service stopped!"
+}
+
+function start_sunray_service() {
+  if [[ `pidof sunray` != "" ]]; then
+        echo "Sunray linux app already running! Exiting..."
+        exit
+  fi
+  # enable sunray service
+  echo "starting sunray service..."
+  #ln -s /home/pi/sunray_install/config_files/sunray.service /etc/systemd/system/sunray.service
+  cp $PWD/config_files/sunray/sunray.service /etc/systemd/system/sunray.service
+  chmod 644 /etc/systemd/system/sunray.service
+  mkdir -p /boot/sunray
+  chmod 644 /boot/sunray
+  systemctl daemon-reload
+  systemctl enable sunray
+  systemctl start sunray
+  systemctl --no-pager status sunray
+  echo "sunray service started!"
+}
+
+function stop_sunray_service() {
+  # disable sunray service
+  echo "stopping sunray service..."
+  systemctl stop sunray
+  systemctl disable sunray
+  echo "sunray service stopped!"
+}
+
+function start_log_service(){
+  echo "starting logging service..."
+  service rsyslog start
+}
+
+function stop_log_service(){
+  echo "stopping logging service..."
+  service rsyslog stop
+}
+
+function start_dm(){
+  echo "starting display manager..."
+  service lightdm start
+}
+
+function stop_dm(){
+  echo "stopping display manager..."
+  service lightdm stop
+}
+
+function list(){
+  echo "listing services..."
+  service --status-all
+}
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo)"
+  exit
+fi
+
+if [ ! -d "/etc/motion" ]; then
+  echo installing motion...
+  apt install motion
+fi
+
+
+if [ ! -d "/home/pi/sunray_install" ]; then
+  echo "run install.sh first!"
+  exit
+fi
+
+#systemctl status sunray
+#systemctl status motion
+
+
+# show menu
+PS3='Please enter your choice: '
+options=("Start sunray service" 
+  "Stop sunray service" 
+  "Start camera service"  
+  "Stop camera service"
+  "Start logging service"
+  "Stop logging service"
+  "Start display manager"   
+  "Stop display manager"   
+  "List services"   
+  "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Start sunray service")
+            start_sunray_service
+            break
+            ;;
+        "Stop sunray service")
+            stop_sunray_service
+            break
+            ;;            
+        "Start camera service")
+            start_cam_service
+            break
+            ;;
+        "Stop camera service")
+            stop_cam_service
+            break
+            ;;
+        "Start logging service")
+            start_log_service
+            break
+            ;;
+        "Stop logging service")
+            stop_log_service
+            break
+            ;;
+        "Start display manager")
+            start_dm
+            break
+            ;;                                        
+        "Stop display manager")
+            stop_dm
+            break
+            ;;                                        
+        "List services")
+            list
+            break
+            ;;                                        
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
---
+++ alfred/start_sunray.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "EUID=$EUID"
+echo "PWD=$PWD"
+
+CMD=""
+
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo)"
+  exit
+fi
+
+if [ ! -d "/home/pi/Sunray/alfred/build" ]; then
+  echo "install Sunray first!"
+  exit
+fi
+
+if [[ `pidof sunray` != "" ]]; then
+  echo "Sunray linux app already running! Exiting..."
+  #exit
+fi
+
+
+# -----------------------------------------
+echo "----bluetooth devices----"
+hcitool dev
+# configure bluetooth BLE module
+echo "----BLE config----"
+echo 12 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval  # 24   6
+echo 20 > /sys/kernel/debug/bluetooth/hci0/conn_max_interval  # 40   6
+echo 1 > /sys/kernel/debug/bluetooth/hci0/conn_latency       # 0    1
+btmgmt -i hci0 power off
+btmgmt -i hci0 le on
+btmgmt -i hci0 bredr off
+btmgmt -i hci0 connectable on
+#btmgmt -i hci0 name "bananapi $MAC"
+btmgmt -i hci0 name "bananapi"
+btmgmt -i hci0 advertising on
+btmgmt -i hci0 power on
+## btmgmt -i hci0 advinfo
+# -----------------------------------------
+
+
+echo "----waiting for TCP connections to be closed from previous sessions----"
+echo "Waiting TCP port 80 to be closed..."
+for _ in `seq 1 30`; do 
+  RES=$(netstat -ant | grep -w 80)
+  #RES=$(lsofs -i:80)
+  #RES=$(fuser 80/tcp) 
+  if [ -z "$RES" ]; then
+    break
+  fi
+  echo $RES
+  # echo -n .  
+  sleep 2.0     
+done; 
+
+
+echo "----starting sunray----"
+echo "CMD=$CMD"
+    
+cd /boot/sunray
+
+
+/usr/bin/stdbuf -oL -eL /home/pi/Sunray/alfred/build/sunray
+
+
+# debug mode
+# exec gdbserver :1234 /home/pi/sunray_install/sunray "$@"
+# debug executable with:  gdb /home/pi/sunray_install/sunray
+
