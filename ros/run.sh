89d507c8adec9cdadced39e656ec97fe0e817e1e
greymfm <pi@testpi5.local>
2024-06-27 17:36:33 +0200

    adjust run.sh

--- ros/run.sh
+++ ros/run.sh
@@ -5,12 +5,17 @@
 cd build
 cmake ..
 make
-cd ..
+
+cd build
+
+
+# setup CAN bus
+ip link set can0 up type can bitrate 1000000    
 
 
 # run Sunray ROS node 
 sudo setcap 'cap_net_bind_service=+ep' devel/lib/sunray_node/sunray_node
-
+. devel/setup.bash
 roslaunch sunray_node test.launch 
 
 
