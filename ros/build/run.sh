cf03c7f5ae1916cbbd4558a5c04e2ab8f22e5317
greymfm <alex@grauonline.de>
2024-06-27 12:43:53 +0200

    add run.sh

--- ros/build/run.sh
+++ ros/build/run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 
+# build Sunray ROS node
+cmake ..
+make
+
+
 # run Sunray ROS node 
 sudo setcap 'cap_net_bind_service=+ep' devel/lib/sunray_node/sunray_node
 
 roslaunch sunray_node test.launch 
+
 
 
