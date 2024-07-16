9a03f00599fb413a8c588559f18dd061ca887af6
greymfm <grau@owlrobotics.de>
2024-07-16 13:42:13 +0200

    update ros/run.sh

--- ros/run.sh
+++ ros/run.sh
@@ -10,6 +10,7 @@ cd build
 
 
 echo "----bluetooth devices----"
+systemctl enable bluetooth.service
 hcitool dev
 # configure bluetooth BLE module
 echo "----BLE config----"
@@ -32,6 +33,39 @@ ip link set can0 up type can bitrate 1000000
 echo "---------all processes using the CAN bus------------"
 sudo lsof | grep -i can_raw
 echo "----------------------------------------------------"
+
+
+
+# setup audio interface
+# https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/SystemWide/
+if ! command -v play &> /dev/null
+then 
+  echo "installing audio player..."
+  apt install -y libsox-fmt-mp3 sox mplayer
+fi
+# show audio devices
+aplay -l
+# restart pulseaudio daemon as root
+killall pulseaudio
+pulseaudio -D --system --disallow-exit --disallow-module-loading
+# set default volume 
+amixer -D pulse sset Master 100%
+
+
+echo "----waiting for TCP connections to be closed from previous sessions----"
+echo "Waiting TCP port 80 to be closed..."
+for _ in `seq 1 20`; do 
+  RES=$(netstat -ant | grep -w 80)
+  #RES=$(lsofs -i:80)
+  #RES=$(fuser 80/tcp) 
+  if [ -z "$RES" ]; then
+    break
+  fi
+  echo $RES
+  # echo -n .  
+  sleep 2.0     
+done; 
+
 
 
 # run Sunray ROS node 
