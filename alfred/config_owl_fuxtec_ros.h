e54eddf35a79153387c7bccc96f2c8a9c0400144
greymfm <alexandergrau@gmx.de>
2024-07-30 18:24:24 +0200

    fix launch files

--- alfred/config_owl_fuxtec_ros.h
+++ alfred/config_owl_fuxtec_ros.h
@@ -300,7 +300,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 //#define CURRENT_FACTOR 1.98   // PCB1.4 (non-bridged INA169, max. 2.5A)
 //#define CURRENT_FACTOR 2.941  // PCB1.4 (bridged INA169, max. 5A)
 
-#define GO_HOME_VOLTAGE   25.5  // start going to dock below this voltage
+#define GO_HOME_VOLTAGE   22.5  // start going to dock below this voltage
 // The battery will charge if both battery voltage is below that value and charging current is above that value.
 #define BAT_FULL_VOLTAGE  30.0  // start mowing again at this voltage
 #define BAT_FULL_CURRENT  -0.1   // start mowing again below this charging current (amps)
