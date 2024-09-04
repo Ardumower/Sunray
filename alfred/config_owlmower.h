f98701576cd8a08a1333b42fa1e753d4bde6e477
greymfm <grau@owlrobotics.de>
2024-09-04 13:39:12 +0200

    can: charger

--- alfred/config_owlmower.h
+++ alfred/config_owlmower.h
@@ -415,9 +415,9 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 // ----- other options --------------------------------------------
 
 // button control (turns on additional features via the POWER-ON button)
-#define BUTTON_CONTROL true      // additional features activated (press-and-hold button for specific beep count: 
+//#define BUTTON_CONTROL true      // additional features activated (press-and-hold button for specific beep count: 
                                  //  1 beep=stop, 6 beeps=start, 5 beeps=dock, 3 beeps=R/C mode ON/OFF)
-//#define BUTTON_CONTROL false   // additional features deactivated
+#define BUTTON_CONTROL false   // additional features deactivated
 
 //#define USE_TEMP_SENSOR true  // only activate if temp sensor (htu21d) connected
 #define USE_TEMP_SENSOR false  
