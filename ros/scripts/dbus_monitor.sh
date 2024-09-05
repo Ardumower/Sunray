6a17c5211805ac4d18f1d819aa5bff2a7177c8d5
greymfm <alex@grauonline.de>
2024-09-05 15:15:23 +0200

    update dbus scripts

--- ros/scripts/dbus_monitor.sh
+++ ros/scripts/dbus_monitor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# dbus-send --system --dest=de.sunray.Bus --print-reply \
-#      /de/sunray/Bus de.sunray.Bus.Play string:"/path/to/yourfile.mp3"
+# dbus-send --system --dest=de.sunray.Bus --print-reply /de/sunray/Bus de.sunray.Bus.Play string:"/home/alex/Sunray/tts/de/testing_audio.mp3"
 
 
 sudo killall dbus-monitor
