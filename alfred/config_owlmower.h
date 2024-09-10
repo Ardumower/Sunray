31c24e9432929ec602b0753a8ab2cc5412af5061
greymfm <alex@grauonline.de>
2024-09-10 20:13:31 +0200

    config: add option DOCK_DETECT_OBSTACLE_IN_DOCK

--- alfred/config_owlmower.h
+++ alfred/config_owlmower.h
@@ -401,7 +401,7 @@ Also, you may choose the serial port below for serial monitor output (CONSOLE).
 #define DOCK_APRIL_TAG 1         // use visual (april-tag) docking?
 #define DOCK_LINEAR_SPEED 0.05   // linear speed for docking
 
-#define DOCK_DETECT_OBSTACLE_IN_DOCK true   // enable obstacle detection in dock?
+#define DOCK_DETECT_OBSTACLE_IN_DOCK false   // enable obstacle detection in dock?
 
 // ---- path tracking -----------------------------------
 
