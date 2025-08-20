db0472650e1a5caab2e8529850dcb97bfc2cf2d4
Alexander Grau <alexandergrau@gmx.de>
2025-08-20 16:58:35 +0200

    relay driver update

--- sunray/src/driver/CanRobotDriver.cpp
+++ sunray/src/driver/CanRobotDriver.cpp
@@ -1017,7 +1017,7 @@ void CanRelaisDriver::setRelaisState(int relais_node_id, bool state){
 
   canRobot.sendCanData(OWL_RELAIS_MSG_ID, relais_node_id, can_cmd_set, owlrls::can_val_relais_state, data);
   CONSOLE.print(" Relais is now set to");
-  CONSOLE.print(state);
+  CONSOLE.println(state);
 }
 
 
