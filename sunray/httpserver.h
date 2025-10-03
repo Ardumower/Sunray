543452a1e39e49a47298532535b92c85eb1a5a20
greymfm <alex@grauonline.de>
2025-10-03 10:16:08 +0200

    refactor code: Comm

--- sunray/httpserver.h
+++ sunray/httpserver.h
@@ -1,3 +1,10 @@
+// Ardumower Sunray 
+// Copyright (c) 2013-2020 by Alexander Grau, Grau GmbH
+// Licensed GPLv3 for open source use
+// or Grau GmbH Commercial License for commercial use (http://grauonline.de/cms2/?page_id=153)
+
+// robot http (+websocket) interface (app server/cloud client)
+
 #ifndef HTTPSERVER_H
 #define HTTPSERVER_H
 
